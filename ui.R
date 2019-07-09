13489dc8d3610637d5fe57188c6272856752e085
Fernando Souza <fcs.est@gmail.com>
2019-07-09 17:12:29 -0300

    Adicionando função para verificar se os packages estão instalados e se não estiverem, instala e chama eles.

--- ui.R
+++ ui.R
@@ -1,3 +1,19 @@
+ler_libs <- function(packages){
+  # Cria vetor com os nomes dos packages chamados que não estão instalados:
+  instalar <- packages[!(packages %in% installed.packages()[, "Package"])]
+  
+  # Caso exista algum package não instalado na lista:
+  if(length(instalar) > 0){
+    # Instala os packages e suas dependências (packages dos quais eles dependem):
+    install.packages(pkgs = instalar, dependencies = TRUE)
+  }
+  
+  # Aplica a função "require" em cada package, com a opção "character.only = TRUE" para passar os nomes como strings:
+  invisible(sapply(packages, require, character.only = TRUE))
+}
+
+ler_libs(c("shiny", "shinyjs", "xlsx", "shinythemes"))
+
 library(shiny)
 library(shinyjs)
 library(xlsx)
